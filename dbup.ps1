776b8f686f181a91b89582f975497a312ca1d79d
Justwouter <wouter2312@gmail.com>
2022-09-25 22:01:11 +0200

    Added some logic and parameter safety

--- dbup.ps1
+++ dbup.ps1
@@ -1,14 +1,30 @@
-﻿#Easy update script. Use .\dbup {args} or put on PATH
+﻿#Easy update script. Use .\dbup {args} or put in PATH
 
-$migration_name=$args[0]
+
+if($null -eq $args[0]){
+    throw "Please enter a migration name or use the -d parameter for default naming"
+}
+
 $startLocation=Get-Location
 
-$projectLocation=dotnet sln list
+try{
+    if($args[0] -eq "-d"){
+        $migration_name=$null
+    }
+    else {
+        $migration_name=$args[0]
+    }
+    
+    $projectLocation=dotnet sln list
 
-$projectLocationParseFirstResult = $projectLocation -replace '/[^/]*$',""
-$projectLocationParseString = $projectLocationParseFirstResult[2].Substring(0, $projectLocationParseFirstResult[2].lastIndexOf('\'))
+    $projectLocationParseFirstResult = $projectLocation -replace '/[^/]*$',""
+    $projectLocationParseString = $projectLocationParseFirstResult[2].Substring(0, $projectLocationParseFirstResult[2].lastIndexOf('\'))
 
-Set-Location "$projectLocationParseString"
-dotnet ef migrations add $migration_name
-dotnet ef database update
-Set-Location $startLocation
+    Set-Location "$projectLocationParseString"
+    dotnet ef migrations add $migration_name
+    dotnet ef database update
+}
+finally{
+    Set-Location $startLocation
+}
+
