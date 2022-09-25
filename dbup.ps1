0a153406ab1b0af51d8274f2f9f8af7336d080fe
Justwouter <wouter2312@gmail.com>
2022-09-25 22:01:36 +0200

    Restructured the code a bit

--- dbup.ps1
+++ dbup.ps1
@@ -8,20 +8,21 @@ if($null -eq $args[0]){
 $startLocation=Get-Location
 
 try{
-    if($args[0] -eq "-d"){
-        $migration_name=$null
-    }
-    else {
-        $migration_name=$args[0]
-    }
-    
+
     $projectLocation=dotnet sln list
 
     $projectLocationParseFirstResult = $projectLocation -replace '/[^/]*$',""
     $projectLocationParseString = $projectLocationParseFirstResult[2].Substring(0, $projectLocationParseFirstResult[2].lastIndexOf('\'))
 
     Set-Location "$projectLocationParseString"
-    dotnet ef migrations add $migration_name
+
+    if($args[0] -eq "-d"){
+        dotnet ef migrations add
+    }
+    else {
+        $migration_name=$args[0]
+        dotnet ef migrations add $migration_name
+    }
     dotnet ef database update
 }
 finally{
