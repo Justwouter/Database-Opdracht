76683df88f27ceefa4e23bd31c086c4459a7ba62
Justwouter <wouter2312@gmail.com>
2022-09-27 21:33:20 +0200

    Edited the high age lambdas & added some debug stuff

--- src/ormthing/Rapporting/DemografischRapport.cs
+++ src/ormthing/Rapporting/DemografischRapport.cs
@@ -16,7 +16,7 @@ class DemografischRapport : Rapport
             ret += "Alle gasten hebben een reservering\n";
         else
             ret += "Niet alle gasten hebben een reservering\n";
-        ret += $"Het percentage bejaarden is { await PercentageBejaarden() }\n";
+        ret += $"Het percentage bejaarden is { await PercentageBejaarden() }%\n";
 
         ret += $"De oudste gast heeft een leeftijd van { await HoogsteLeeftijd() } \n";
 
@@ -35,10 +35,32 @@ class DemografischRapport : Rapport
     private async Task<int> AantalSinds(DateTime sinds) => context.Guests.Where<Gast>(gast => gast.EersteBezoek > sinds).Count();
     private async Task<Gast> GastBijEmail(string email) => context.Guests.First<Gast>(gast => gast.Email == email); 
     private async Task<Gast?> GastBijGeboorteDatum(DateTime d) => context.Guests.First<Gast>(gast => gast.GeboorteDatum == d);
-    private async Task<double> PercentageBejaarden() => (context.Guests.Where<Gast>(gast => (EF.Functions.DateDiffDay(gast.GeboorteDatum, DateTime.Now) / 365.25)>65).Count()/await AantalGebruikers())*100;
-    private async Task<int> HoogsteLeeftijd() => context.Guests.Select(gast => (int)(EF.Functions.DateDiffDay(gast.GeboorteDatum, DateTime.Now) / 365.25)).Min();
+    private async Task<double> PercentageBejaarden() => ((double)(context.Guests.Where<Gast>(gast => (int)(EF.Functions.DateDiffDay(gast.GeboorteDatum, DateTime.Now)/365.25)>75).Count())/(double)(context.Guests.Count()))*100;
+    private async Task<int> HoogsteLeeftijd() => context.Guests.Select(gast => (int)(EF.Functions.DateDiffDay(gast.GeboorteDatum, DateTime.Now) / 365.25)).Max();
     //private async Task<(string dag, int aantal)[]> VerdelingPerDag() => ;
     private async Task<int> FavorietCorrect() => context.Guests.Where(gast => gast.FavorieteAttractie !=null).Where(gast => gast.reservering.Count() > 0).Where(gast => gast.reservering.Any(r => r.ReservedAttractions.Contains(gast.FavorieteAttractie))).Count();
     //Check if guests have a favorite, check if they have/had reservations and if so, check if their favorite attraction was included in any reservation.
     //Currently don't really have a way to monitor if they have visited their favorite the most tho afaik. unless we go of reservations again but that would be annoying and tedious 
+
+
+
+
+
+
+
+
+
+
+
+    /* Nothing to see here, just debug testing*/
+    // public async Task<int> brithdaysDEBUG() => context.Guests.Where<Gast>(gast => gast.GeboorteDatum < (DateTime.Now).AddYears(-75)).Count();
+    // public async Task<double> brithdaysDEBUGEFC() => ((double)(context.Guests.Where<Gast>(gast => ((int)(EF.Functions.DateDiffDay(gast.GeboorteDatum, DateTime.Now)/365.25))>75).Count())/(double)(context.Guests.Count()))*100;
+    // public async Task<int> UserAmountDEBUG() => context.Guests.Count();
+
+    // public async Task<double> birthdaysDEBUG_EFC_FULL(){
+    //     int senior_amt = context.Guests.Where<Gast>(gast => ((int)(EF.Functions.DateDiffDay(gast.GeboorteDatum, DateTime.Now)/365.25))>75).Count();
+    //     int guest_amt = context.Guests.Count();
+    //     double senior_prct = ((double)senior_amt/(double)guest_amt);
+    //     return senior_prct;
+    // }
 }
